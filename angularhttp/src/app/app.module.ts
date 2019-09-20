af372a3305e6b2cd97e508caace3b4213850475d
damatogabriele <damato.gabriele@iisgalvanimi.edu.it>
2019-09-20 08:54:27 +0000

    15.2

--- angularhttp/src/app/app.module.ts
+++ angularhttp/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; //Importa Il modulo http
 
 import { AppComponent } from './app.component';
-import { FooComponent } from './foo/foo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FooComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
